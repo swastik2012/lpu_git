d041907ced9254247f489aed59a69f995ca04576
Swastik Juneja <swastikjuneja2012@gmail.com>
2023-01-11 15:25:31 +0530

    commit

--- three.dart
+++ three.dart
@@ -1,12 +1,14 @@
 // 3
 
-
 //
 
 void main(List<String> args) {
-  
-}
-
-void printAns() {
-  
+  List<String> strList = ["Pasta | 40.5", "Pizza | 700", "Macroni | 67.5"];
+  strList.map((e) {
+    String name = e.split(" | ")[0].trim();
+    double Price = double.parse(e.split(" | ")[1].trim());
+    double discountedPrice = 0.8 * Price;
+    // print("hi");
+    print("$name is of ${Price} and the discounted amount is $discountedPrice");
+  }).toList();
 }
