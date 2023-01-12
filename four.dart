e3334268e9452a712c63ecbb609795ebcc73c956
Swastik Juneja <swastikjuneja2012@gmail.com>
2023-01-12 10:21:10 +0530

    two.dart updated

--- four.dart
+++ four.dart
@@ -1,4 +1,36 @@
-// 4
+import 'dart:html';
+import 'dart:io';
 
+// pullScreenToFront(1)
+// moveScreen(1, 3)
+// deleteScreen(6)
+// deleteScreen(2)
+// addScreenToFront("FIFA 23")
+// pullScreenToFront(1)
+// addScreenToFront("NFS")
+// moveScreen(1, 3)
+// findScreenAtIndex(2)
 
-//
+void main(List<String> args) {
+  List<String> windowScreens = ['Zoom', 'music', 'chrome', 'notes'];
+}
+
+List pullScreenToFront(int currentIndex, List Screens) {
+  int n = Screens.length;
+  if (currentIndex < n - 1) {
+    String s1 = Screens[0];
+    Screens[0] = Screens[currentIndex];
+    Screens[currentIndex] = s1;
+    return Screens;
+  } else {
+    return Screens;
+  }
+}
+
+void moveScreen(int currentIndex, int destinationIndex) {}
+
+void deleteScreen(int index) {}
+
+void addScreenToFront(String windowName) {}
+
+void findScreenAtIndex(int index) {}
--- two.dart
+++ two.dart
@@ -1,4 +1,39 @@
-// random changes 
+// import 'dart:io';
 
-//
-//
+void main() {
+  printUser(
+    () => 'Hello',
+  );
+
+  printChefName(getChefFromDb, 4);
+
+  // List<String> testStringList = ['Hello', 'cello', 'mello'];
+  // List<String> mappedString = testStringList.map((e) => null);
+}
+
+void printUser(String Function() getUser) {
+  String user = getUser();
+
+  print(user);
+}
+
+void printChefName(String Function(int id) getChefFromDb, int id) {
+  String name = getChefFromDb(id);
+  print(name);
+}
+
+String getChefFromDb(int id) {
+  switch (id) {
+    case 1:
+      return "Shreyash";
+      break;
+    case 2:
+      return "Krishna";
+      break;
+    case 3:
+      return 'Swastik';
+      break;
+    default:
+      return 'No Name FOund';
+  }
+}
